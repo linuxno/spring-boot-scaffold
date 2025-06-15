84ed03186dbabd1720a18ece616b16e8f20b3884
linuxdo <jjliu878@gmail.com>
2025-06-15 23:56:27 +0800

    init

---
+++ src/main/java/cn/code/scaffold/SpringBootScaffoldApplication.java
@@ -0,0 +1,18 @@
+package cn.code.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author jjliu
+ * @version 2025-06-15
+ */
+@SpringBootApplication
+public class SpringBootScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootScaffoldApplication.class, args);
+	}
+
+}
